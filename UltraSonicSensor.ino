05208c6d7ddffe08c57d252475af41b6393b6eb5
Sobhan Haggi <39483396+SOB845@users.noreply.github.com>
2023-08-09 00:10:41 +0330

    Update UltraSonicSensor.ino
    introduced prevTime and currentTime to avoid using delay()

--- UltraSonicSensor.ino
+++ UltraSonicSensor.ino
@@ -1,12 +1,16 @@
-#include <neotimer.h>
+// Progam to control a digital pin using an HC-SR04 ultrasonic sensor via distance measurement
+
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27,16,2);
 
-const byte BasePin = 2;
+const byte BasePin = 2; // I use this pin to control the base of an NPN transistor hence the naming
 const byte trig = 5;
 const byte echo = 7;
 float distance, duration;
-Neotimer limit = Neotimer(10000);
+bool isBaseHigh;
+
+unsigned long prevTime = 0;
+unsigned const TimeLimit = 15000; // How long should BasePin stay HIGH
 
 void setup() {
   lcd.init();
@@ -15,10 +19,11 @@ void setup() {
   pinMode(BasePin, OUTPUT);
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
-  Serial.begin(9600);
 }
 
 void loop() {
+  unsigned long currentTime = millis();
+  
   digitalWrite(trig , LOW);
   delayMicroseconds(2); 
   digitalWrite(trig, HIGH); 
@@ -31,31 +36,35 @@ void loop() {
   Serial.println(distance);
   interrupts();
 
+  lcd.setCursor(3,0);
+  lcd.print("DISTANCE:");
+  lcd.setCursor(2,1);
+  lcd.print(String(distance));
+  lcd.setCursor(10,1);
+  lcd.print("cm");
+  delay(60);
 
-  if(distance < 230.0){
-    limit.start();
-    lcd.setCursor(1,0);
-   
-    while(limit.waiting()){
-      digitalWrite(BasePin,HIGH);
-      if (limit.done()){
-        limit.stop();
-        digitalWrite(BasePin, LOW);
+  // The distance unit is in centimeters, which I set its threshold to be 270 cm
+  // You may want to change it
+  if(distance < 270.0){
+    if (digitalRead(BasePin)== LOW){
+      digitalWrite(BasePin, HIGH);
+      isBaseHigh = true;      
       }
-      break;
-      }
-    //lcd.clear();
-    }
       
+    if (currentTime - prevTime >= TimeLimit){
+      // If time difference between currentTime and prevTime is more than 15 secs...
+      digitalWrite(BasePin, LOW);
+      isBaseHigh = false;
+      prevTime = currentTime; // Update prevTime (originally set to 0) to be 15s in the first iteration, 30 in the second & so on
+      // This allows the BasePin to stay HIGH for TimeLimit without using delay()
+      }
+  }    
   else{
-    digitalWrite(BasePin,LOW);
-    lcd.setCursor(3,0);
-    lcd.print("DISTANCE:");
-    lcd.setCursor(2,1);
-    lcd.print(String(distance));
-    lcd.setCursor(10,1);
-    lcd.print("cm");
-    delay(60);
+    if (isBaseHigh == true && (currentTime - prevTime) >= TimeLimit){
+      digitalWrite(BasePin, !isBaseHigh);
+      }
+    
    }
   
 }
